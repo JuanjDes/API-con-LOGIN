4895e7aa742d580b56a8d30287501eba5c069b94
dianat-prog <dianatprogramadora@gmail.com>
2025-01-25 19:51:14 +0100

    añado cambios de config.js y datos de session"

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
-
 const app = express();
 
+const PORT =3000;
+const usersRoutes=require('./routes/users.js')
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const { jwtSecret } = require('./crypto/config'); // Asegúrate de importar un secreto seguro
 
+
+// Middleware para manejar datos de formularios URL-encoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware
+app.use(bodyParser.json());
+
+//- Sesión
+app.use(
+    session({
+      secret:jwtSecret, // Clave secreta para firmar el token (debería ser segura, preferiblemente generada con crypto)
+      resave: false, // No guardar cambios en la sesión siempre, solo cuando se realice algún cambio
+      saveUninitialized: true, // Se guarda la inicialización de la sesión
+      cookie: { secure: false }, // Cambia a 'true' si estás utilizando HTTPS
+    })
+  );
+
+  app.use('/',usersRoutes)//Ruta de usuarios
 
 // escuchamos en puerto 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Servidor escuchando en el puerto 3000');
 });
